54f75c3def846a53512dcb7f6797c56d8fd88577
Misiek40k <comanderrr@gmail.com>
2020-03-04 23:48:17 +0100

    Add identity generation loop

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const peaople = [];
+const people = [];
 const genders = ['male', 'female'];
 const maleNames = ['Cody', 'Rex', 'Anakin', 'Maul'];
 const femaleNames = ['Padme', 'Leia', 'Shmi', 'Rey'];
@@ -11,3 +11,29 @@ const randChoice = (arr) => {
     const randItem = arr[randIndex];
     return randItem;
 }
+
+for (let i = 0; i < 20; i++) {
+    const randGender = randChoice(genders);
+    let name;
+
+    (randGender === 'male')
+        ?
+        name = randChoice(maleNames)
+        :
+        name = randChoice(femaleNames);
+
+
+    const person = {
+        gender: randGender,
+        firstName: name,
+        lastName: randChoice(lastNames),
+        age: Math.floor(Math.random() * 61) + 18,
+    };
+
+    people.push(person);
+};
+
+fs.writeFile('people.json', JSON.stringify(people), (err) => {
+    if (err) throw ('Something went wrong: ', err);
+    console.log('The file has been saved.');
+});
